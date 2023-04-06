6b871f074d8409cc612223775a02e11ae32771a7
黃雅凡 <ripple@huangyafandeMacBook-Pro.local>
2023-04-07 01:42:48 +0800

    init commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+$(function (){
+    for(let work=0;work<=11;work++){
+        $("#project"+work).click(function(){
+            $(".index").hide();
+            $(".project"+work).show();
+            $(".project"+work).addClass("fade-in-bottom");
+            $("html,body").animate({scrollTop: 0,},0);
+            /* 讓出現的頁面可以從最頂端開始瀏覽，第一個0是位置，第二個是秒數*/
+        });
+
+        $(".back"+work).click(function(){
+            $(".index").show();
+            $(".project"+work).hide();
+        });
+
+    }
+
+
+    $(".gototop").click(function(){
+        $("html,body").animate({scrollTop: 0,},600);
+        /* 讓出現的頁面可以從最頂端開始瀏覽，第一個0是位置，第二個是秒數*/
+    });
+
+
+
+
+
+});
